7c90dc4524e4658f2a4a9d680b5dc6705f83651e
d.florentina.calapod <>
2023-01-18 13:57:36 +0100

    Flutter Themes+init project

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
       home: InputPage(),
     );
   }
@@ -27,6 +33,7 @@ class _InputPageState extends State<InputPage> {
         child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
